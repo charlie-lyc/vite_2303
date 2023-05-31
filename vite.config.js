e3f10aee6478226de3f6bb0db444feead9d9b8fe
YoungCheol Lee <charlie@YoungCheols-MacBook-Pro.local>
2023-05-31 22:35:15 +0900

    note vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-
     /**
      * My Configs...
      */
     server: {
+        // Set client port
         port: 3000,
+        // Set proxy server
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://localhost:3001',
+        //         changeOrigin: true,
+        //     },
+        // },
     },
 })
